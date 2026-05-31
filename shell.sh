32b2e209c7a6b501288650c9c9b4e5ae20b55361
satish <satishkawal9923@gmail.com>
2026-05-31 06:26:12 +0000

    second commit

---
+++ shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+cp hello.html /var/www/html 
+sudo systemctl restart nginx
+
+echo "devboard runing on port 80"
